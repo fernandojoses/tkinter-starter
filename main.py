c049d5526f94750e5403d82418ee7af6f0685e6a
Fernando Sagastume Sagastume <fjsaga60>
2020-01-28 11:12:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
 # Add a label with the text "Hello"
-lbl = Label(window, text="Hello, This is a new window!")
+lbl = Label(window, text="Hello, This is a new window! There is isn't really much more interesting on here, so you may now close it!")
 
 # Place the label in the window at 0, 0
 lbl.grid(column=0, row=0)
