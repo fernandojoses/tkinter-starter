81a5b554a2400f7d82881a602742472114c9103b
Fernando Sagastume Sagastume <fjsaga60>
2020-02-17 10:34:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -94,39 +94,3 @@ btn.grid(column=0,row=9)
 
 
 window.mainloop()     # Keep the window open
-
-
-import pygame
-import sys
-import random
-import time
-
-class Snake():
-  def __init__(self):
-    self.position = [100,50]
-    self.body = [[100,50],[90,50],[80,50]]
-    self.direction = "RIGHT"
-    self.chnageDirectionTo = self.direction
-
-  def changeDirTo(self,dir):
-      if dir== "RIGHT" and not self.direction== "LEFT":
-    self.direction = "RIGHT"
-      if dir== "LEFT" and not self.direction== "RIGHT":
-      self.direction = "LEFT"
-      if dir== "UP" and not self.direction== "DOWN":
-      self.direction = "UP"
-      if dir== "DOWN" and not self.direction== "UP":
-      self.direction = "DOWN"
-
-  def move(self,foodPos):
-    if self.direction == "RIGHT":
-      self.position[0] += 10
-    if self.direction == "LEFT":
-      self.position[0] -= 10
-    if self.direction == "UP":
-      self.position[1] += 10
-    if self.direction == "UP":
-      self.position[1] += 10
-
-    
-    
